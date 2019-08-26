44210c3dbf140cb876ac124c014e10ff82e184a4
SHOO <zan77137@nifty.com>
2019-08-26 23:02:32 +0900

    Add CI and document deploy settings

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-function if_error_exit() {
-    exit 1
-}
+set -eux -o pipefail
 
-dub test --arch=$ARCH || if_error_exit
-dub test :stmtest --arch=$ARCH || if_error_exit
+TEST_TARGET_ARCH=${TEST_TARGET_ARCH:-x86_64}
+echo "--coverage_dir=${COVERAGE_DIR:-.cov}">.coverageopt
+echo "--coverage_merge=${COVERAGE_MERGE:-true}">>.coverageopt
+DMD=${DMD:-dmd}
+
+dub test -a=${TEST_TARGET_ARCH} -b=unittest-cov -c=ut --compiler=${DMD}
+dub test :stmtest -a=${TEST_TARGET_ARCH} -b=unittest-cov --compiler=${DMD}
